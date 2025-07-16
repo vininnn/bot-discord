a070a77e17b367cfc648b82bf453f9084265fd24
Vinicius Herberts Dal Bem <viniciusherberts@gmail.com>
2025-07-16 17:02:06 -0300

    Rework no código + Comandos novos
    Código divido em módulos com um grupo de funções e comandos separados. Função de ver quanto tempo uma lição ficou. Função de motivar e inspirar um membro triste.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
-
-from datetime import datetime, timezone
+from sessions import register_study_sessions
+from quotes import register_quotes
+from quotes import on_message
 import os
 
 from dotenv import load_dotenv
@@ -11,51 +11,16 @@ load_dotenv()
 intents = discord.Intents.all()
 bot = commands.Bot('!', intents=intents)
 
+bot.add_listener(on_message)
+
 # Bot initialization
 @bot.event
 async def on_ready():
+    register_study_sessions(bot.tree)
+    register_quotes(bot.tree)
     synced = await bot.tree.sync()
     print(f'Synchro: {len(synced)}')
     print('All right!')
-
-study_sessions = {}
-
-# Format the study duration (HH:MM:SS)
-def format_study_duration(study_duration):
-    total_seconds = int(study_duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-
-    return f'{hours:02}:{minutes:02}:{seconds:02}' 
-
-# Start a study session
-@bot.tree.command(name='start_studying', description='Start a timer for a specific study subject')
-@app_commands.describe(topic='Study subject')
-async def start_studying(interaction: discord.Interaction, topic: str):
-    await interaction.response.defer()
-
-    if topic in study_sessions:
-        await interaction.followup.send(f'There is already a session named "{topic}" in progress!')
-        return
-
-    study_sessions[topic] = datetime.now(timezone.utc)
-    await interaction.followup.send(f'Timer started successfully! Study subject: "{topic}"')
-
-# End a study session
-@bot.tree.command(name='finish_studying', description='Finish a timer for a specific study subject already initialized')
-@app_commands.describe(topic='Finish subject')
-async def finish_studying(interaction: discord.Interaction, topic: str):
-    await interaction.response.defer()
-
-    if topic not in study_sessions:
-        await interaction.followup.send(f'There are no timers named "{topic}"!')
-        return
-
-    start_time = study_sessions.pop(topic)
-    study_duration = datetime.now(timezone.utc) - start_time
-    formatted_study_duration = format_study_duration(study_duration)
-    await interaction.followup.send(f'Timer finished successfully! Time spent on "{topic}":  {formatted_study_duration}')
 
 
 bot.run(os.getenv('TOKEN'))
---
+++ sessions.py
@@ -0,0 +1,64 @@
+import discord
+from discord import app_commands
+from datetime import datetime, timezone
+
+study_sessions = {}
+ended_sessions = {}
+
+# Format the study duration (HH:MM:SS)
+def format_study_duration(study_duration):
+    total_seconds = int(study_duration.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+
+    return f'{hours:02}:{minutes:02}:{seconds:02}' 
+
+# Function that register the commands
+def register_study_sessions(tree: app_commands.CommandTree):
+    # Start a study session
+    @tree.command(name='start', description='Start a timer for a specific study subject')
+    @app_commands.describe(subject='Study subject')
+    async def start(interaction: discord.Interaction, subject: str):
+        await interaction.response.defer()
+
+        if subject in study_sessions:
+            await interaction.followup.send(f'There is already a session named "{subject}" in progress!')
+            return
+
+        study_sessions[subject] = datetime.now(timezone.utc)
+        await interaction.followup.send(f'Timer started successfully! Study subject: "{subject}"')
+
+    # End a study session
+    @tree.command(name='finish', description='Finish a timer for a specific study subject already initialized')
+    @app_commands.describe(subject='Finish subject')
+    async def finish(interaction: discord.Interaction, subject: str):
+        await interaction.response.defer()
+
+        if subject not in study_sessions:
+            await interaction.followup.send(f'There are no timers named "{subject}"!')
+            return
+
+        start_time = study_sessions.pop(subject)
+        study_duration = datetime.now(timezone.utc) - start_time
+
+        if subject not in ended_sessions:
+            ended_sessions[subject] = study_duration
+        else:
+            ended_sessions[subject] += study_duration
+
+        formatted_study_duration = format_study_duration(study_duration)
+        await interaction.followup.send(f'Timer finished successfully! Time spent on "{subject}":  {formatted_study_duration}')
+
+    # Shows the total time per subject
+    @tree.command(name='summary', description='Shows the total hours studied in the subject')
+    @app_commands.describe(subject='Total time on subject')
+    async def summary(interaction: discord.Interaction, subject: str):
+        await interaction.response.defer()
+
+        if subject not in ended_sessions:
+            await interaction.followup.send(f'There are no ended sessions named "{subject}"!')
+            return
+
+        formatted_time = format_study_duration(ended_sessions[subject])
+        await interaction.followup.send(f'Time spent on "{subject}":  {formatted_time}')    
