7b872a4ec9fa0808f7829c925242e694134208a5
Vinicius Herberts Dal Bem <viniciusherberts@gmail.com>
2025-07-17 11:15:33 -0300

    Adicionando a criação de tarefas
    Adicionado comando de criar, terminar e ver tarefas/tasks.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from sessions import register_study_sessions
+from planner import register_planner
 from quotes import register_quotes
 from quotes import on_message
 import os
@@ -17,6 +18,7 @@ bot.add_listener(on_message)
 @bot.event
 async def on_ready():
     register_study_sessions(bot.tree)
+    register_planner(bot.tree)
     register_quotes(bot.tree)
     synced = await bot.tree.sync()
     print(f'Synchro: {len(synced)}')
